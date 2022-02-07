1ccbeeb1f0abf17412fd76651f8c9381f2d73647
hudson2000 <97479558+hudson2000@users.noreply.github.com>
2022-02-07 18:41:02 -0500

    exercises and index

---
+++ index.js
@@ -0,0 +1,8 @@
+let quotes = [
+  '"A Im not to be played with B cause it can get dangerous C these - people I ride with. This moment; we own it" - 2 Chainz and Wiz Kalifa', //0
+  '"Im going to be king of the pirates!" - Luffy', //1
+  '"With great power comes great responsibility" - Aunt May', //2
+];
+
+//Choose between three integers randomly
+console.log(quotes[Math.floor(Math.random() * 3)]); //0 -- 2.9999999
