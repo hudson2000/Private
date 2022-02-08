c44b0c39a10cc1b232e1d72839b2764524801a5f
hudson2000 <97479558+hudson2000@users.noreply.github.com>
2022-02-07 19:45:10 -0500

    Exercises

--- exercises.js
+++ exercises.js
@@ -3,11 +3,19 @@
 let arrays = [[1, 2, 3], [4, 5], [6]];
 // Your code here.
 // → [1, 2, 3, 4, 5, 6]
+export function flattening(inputList) {
+  return inputList.reduce((ae1, ae2) => ae1.concat(ae2));
+}
+console.log(flattening(arrays));
 
 //3.2 Write a higher-order function loop that provides something like a for loop statement. It takes a value, a test function, an update function, and a body function. Each iteration, it first runs the test function on the current loop value and stops if that returns false. Then it calls the body function, giving it the current value. Finally, it calls the update function to create a new value and starts from the beginning.
 //When defining the function, you can use a regular loop to do the actual looping.
-
 // Your code here.
+export function loop(value, test, update, body) {
+  for (let curValue = value; test(curValue); curValue = update(curValue)) {
+    body(curValue);
+  }
+}
 loop(
   3,
   (n) => n > 0,
@@ -23,6 +31,11 @@ loop(
 
 function every(array, test) {
   // Your code here.
+  let returnValue = true;
+  for (let item of array) {
+    returnValue = returnValue && test(item);
+  }
+  return returnValue;
 }
 
 console.log(every([1, 3, 5], (n) => n < 10));
@@ -31,14 +44,3 @@ console.log(every([2, 4, 16], (n) => n < 10));
 // → false
 console.log(every([], (n) => n < 10));
 // → true
-
-//3.4 Write a function that computes the dominant writing direction in a string of text. Remember that each script object has a direction property that can be "ltr" (left to right), "rtl" (right to left), or "ttb" (top to bottom).
-//The dominant direction is the direction of a majority of the characters that have a script associated with them. The characterScript and countBy functions defined earlier in the chapter are probably useful here.
-function dominantDirection(text) {
-  // Your code here.
-}
-
-console.log(dominantDirection("Hello!"));
-// → ltr
-console.log(dominantDirection("Hey, مساء الخير"));
-// → rtl
